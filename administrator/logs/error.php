1a1a7d8349aa7060fc352e32f6a8e3c8a7eb7219
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2016-11-20 00:29:15 +0530

    lastt

---
+++ administrator/logs/error.php
@@ -0,0 +1,19 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-12 02:33:31 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-12T02:33:30+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T02:34:45+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2016-11-12T02:35:01+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T02:35:14+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T02:46:55+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T02:47:06+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T02:47:14+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2016-11-12T02:47:26+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T02:48:47+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T02:48:55+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-12T05:57:57+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T13:53:02+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2016-11-15T17:47:47+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
