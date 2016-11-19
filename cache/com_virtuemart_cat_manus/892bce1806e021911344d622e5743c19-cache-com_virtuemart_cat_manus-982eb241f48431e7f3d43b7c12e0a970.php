7a25bcc40b8468948324c876582fcdc6ed9ffa59
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2016-11-19 23:36:56 +0530

    part5

---
+++ cache/com_virtuemart_cat_manus/892bce1806e021911344d622e5743c19-cache-com_virtuemart_cat_manus-982eb241f48431e7f3d43b7c12e0a970.php
@@ -0,0 +1,1 @@
+<?php die("Access Denied"); ?>#x#a:2:{s:6:"output";s:0:"";s:6:"result";a:3:{i:0;O:8:"stdClass":2:{s:7:"mf_name";s:7:"Default";s:26:"virtuemart_manufacturer_id";s:1:"2";}i:1;O:8:"stdClass":2:{s:7:"mf_name";s:12:"Manufacturer";s:26:"virtuemart_manufacturer_id";s:1:"1";}i:2;O:8:"stdClass":2:{s:7:"mf_name";s:8:"Producer";s:26:"virtuemart_manufacturer_id";s:1:"3";}}}
